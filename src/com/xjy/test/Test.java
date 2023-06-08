cfbf39417091c783e351bec8056b719fea02c9e6
云 <xjy_jiayou@163.com>
2023-06-09 03:05:53 +0800

    test

---
+++ src/com/xjy/test/Test.java
@@ -0,0 +1,11 @@
+package com.xjy.test;
+
+/**
+ * @Author 云
+ * @create 2023/6/9
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
